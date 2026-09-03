f3b493bd85c3cf636ee5874fb2277d561fd18cd8
Djim Lo <6243876@cegepmontpetit.ca>
2026-09-03 11:05:36 -0400

    FCT View Details ZombieType Nombre Zombie et Moyenne Points
    Ajout du nombre de zombies et la moyenne des points pour le Type de zombie dans la View Details.

--- ZombieParty/ZombieParty/ViewModels/ZombieTypeVM .cs
+++ ZombieParty/ZombieParty/ViewModels/ZombieTypeVM .cs
@@ -6,6 +6,10 @@ namespace ZombieParty.ViewModels
     {
         public ZombieType ZombieType { get; set; }
         public List<Zombie> ZombiesList { get; set; } = new List<Zombie>();
+        //  Que devez-vous modifier afin de présenter le nombre de zombies et la moyenne des points de la liste de zombies pour chaque ZombieType?
+        public int ZombiesCount { get; set; }
+        public double PointsAverage { get; set; }
+
 
     }
 }
